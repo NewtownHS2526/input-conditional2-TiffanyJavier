b10fcf6e91cffd08bda79f603bebac5f954c511b
TiffanyJavier <Tiffanyj78@nycstudents.net>
2026-02-02 14:18:48 -0500

    input-conditional2

--- script.js
+++ script.js
@@ -23,9 +23,9 @@
 //   3. The status message area (id="status")
 // --------------------------------------------
 
-const textBox = 
-const outputBox = 
-const statusBox = 
+const textBox = document.querySelector("#user-inputs");
+const outputBox = document.querySelector("#quiz-outputs");
+const statusBox = document.querySelector("#status");
 
 // This console.log helps us verify our selections worked correctly.
 // Open the browser's Developer Tools (F12) to see the output.
@@ -43,8 +43,8 @@ console.log(textBox, outputBox, statusBox);
 //   2. A "bonus" variable (boolean) initialized to false
 // --------------------------------------------
 
-let score = 
-let champlain = 
+let score = 0
+let champlain = false;
 
 // --------------------------------------------
 // STEP 3: CREATE THE ANSWER-CHECKING FUNCTION
@@ -73,21 +73,27 @@ const checkAnswer = () => {
   //   - Display an error message in the statusBox
 
   if (currentAnswer === "Lake Superior" || currentAnswer === "Superior") {
-
+    outputBox.innerHTML += `<h3>Lake Superior</h3>`
+    score += 0;
   } else if (currentAnswer === "Lake Michigan" || currentAnswer === "Michigan") {
-   
+    outputBox.innerHTML += `<h3>Lake Michigan</h3>`
+    score += 0;
   } else if (currentAnswer === "Lake Huron" || currentAnswer === "Huron") {
-  
+    outputBox.innerHTML += `<h3>Lake Huron</h3>`
+    score += 0;
   } else if (currentAnswer === "Lake Erie" || currentAnswer === "Erie") {
-    
+    outputBox.innerHTML += `<h3>Lake Erie</h3>`
+    score += 0;
   } else if (currentAnswer === "Lake Ontario" || currentAnswer === "Ontario") {
-
+    outputBox.innerHTML += `<h3>Lake Ontario</h3>`
+    score += 0;
   } else if (currentAnswer === "Lake Champlain" || currentAnswer === "Champlain") {
     // Secret bonus answer!
-  
+    outputBox.innerHTML += `<h3>Lake Champlain</h3>`
+    Champlain += false;
   } else {
     // If no conditions match, show an error message
-    
+    statusBox.innerHTML = `sorry, but ${currentAnswer} is not one of the great lakes`
   }
 
   // After checking the answer, verify if the game is complete
@@ -111,13 +117,13 @@ const checkAnswer = () => {
 
 const checkScore = () => {
   if (score === 5) {
- 
+    statusBox.innerHTML = "Congratulations! You found all five Great Lakes!";
   }
   if (score === 5 && champlain) {
-    
+    statusBox.innerHTML = "Five Great Lakes in America";
     // Disable the text box since the game is complete
-    
-  }
+    textBox.disabled = true;
+  } 
 };
 
 // --------------------------------------------
@@ -138,3 +144,4 @@ const checkScore = () => {
 //   - Calls the checkAnswer function when triggered
 // --------------------------------------------
 
+textBox.addEventListener("Change", checkAnswer);
